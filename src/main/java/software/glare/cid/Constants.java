d7fc53dcf75cecde485bfe4655aa3b2c1a30c38b
fdman <fdman@mail.ru>
2015-04-27 00:13:45 +0400

    ...

--- src/main/java/software/glare/cid/Constants.java
+++ src/main/java/software/glare/cid/Constants.java
@@ -5,8 +5,8 @@ package software.glare.cid;
  * Date: 01.12.13
  */
 public class Constants {
-    public static final int CPU_CORES_NUM = 4;
-    public static final int INPUT_QUEUE_SIZE_NUM = 10;
+    public static final int CPU_CORES_NUM = 4+1;
+    public static final int INPUT_QUEUE_SIZE_NUM = (int) (CPU_CORES_NUM*1.2);
     public static final int PROCESSING_WAIT_SECONDS = 10;
     public static final String PROCESSED_FILES_NEW_EXTENSION = "cid";
 
